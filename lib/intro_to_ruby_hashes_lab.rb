c9deeead843f795bad4546836538a12af56b18c6
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-17 19:03:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,52 @@ def new_hash
 end
 
 def my_hash
-  {has: "carpet"}
+  {:key => "value"}
 end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  {name: "Grace Hopper"}
+def pioneer 
+  {:name => "Grace Hopper"}
 end
 
 
-def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
-  {id: number}
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def new_hash
+#   {}
+# end
+
+# def my_hash
+#   {has: "carpet"}
+# end
+
+# def pioneer
+#   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+#   {name: "Grace Hopper"}
+# end
+
+
+# def id_hash_generator(number)
+#   # return a hash with a key :id assigned to the provided number
+#   {id: number}
+# end
